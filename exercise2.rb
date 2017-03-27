e30bfc4f92526322f2a43eb22afa926b3848fbaa
Swapna Brungi <swapna.brungi@gmail.com>
2017-03-27 13:58:07 -0400

    exercise 2 added with 4 questions

---
+++ exercise2.rb
@@ -0,0 +1,18 @@
+#1)the tip for 55$
+x = 55
+if x % 0.13
+  puts ( "The tip amount is #{x * 0.20} dollars" )
+end
+
+
+#2)converting the integer to string and adding them
+if x = 2.to_s + "two"
+puts x
+end
+
+#3)multiplication in a string interpolation
+puts (" the result is #{45628 * 7839}")
+
+
+#4)this should be true
+puts (10 < 20 && 30 < 20) || !(10 == 11)
